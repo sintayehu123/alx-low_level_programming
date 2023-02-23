cb6c68b12394f1a2a9a999f59cdebe920dd4125a
sintayehu123 <sintayehumolla@gmail.com>
2023-02-23 06:54:00 -0800

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,8 +2,8 @@
 
 /**
  *  mul - a function that multiplies two integers
- * @a: first input number
- * @b: second input number 
+ * @a: first input
+ * @b: second input 
  * Return: multiplication result of the 2 inputs
  */
 int mul(int a, int b)

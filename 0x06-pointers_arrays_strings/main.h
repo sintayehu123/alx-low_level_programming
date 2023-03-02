15463ffeaab2697960f70ebae0bd56eccd6eb131
sintayehu123 <sintayehumolla@gmail.com>
2023-03-02 13:37:43 -0800

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,3 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
-
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -11,6 +8,7 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
-char *_strstr(char *haystack, char *needle);
 void print_number(int n);
-#endif
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+int main(void);

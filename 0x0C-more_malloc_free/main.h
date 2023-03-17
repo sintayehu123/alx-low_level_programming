5abe9711f0122cac3169ab196036321e1e86fac6
sintayehu123 <sintayehumolla@gmail.com>
2023-03-17 07:28:52 -0700

    main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdio.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
@@ -9,4 +9,5 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-#endif
+
+#endif /* MAIN_H */

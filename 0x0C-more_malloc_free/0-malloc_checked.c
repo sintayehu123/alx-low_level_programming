7bbef5c5eb186d0db71c44815c1f3d3a8d9911fe
sintayehu123 <sintayehumolla@gmail.com>
2023-03-17 06:40:31 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *malloc_checked - program startup
+ * @b: int being evaluated
+(*
+ * Description: allocates memory using malloc)?
+ * Return: return (0) is the required function signature
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *memory = malloc(b);
+
+	if (memory == NULL)
+	{
+		exit(98);
+	}
+	return (memory);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,6 @@
+ 0x0E. C - More malloc, free
+Tasks📓
+0-malloc_checked.c: write a function that allocates memory using malloc
+1-string_nconcat.c: write a function concatenates two strings
+2-calloc.c: write a function that allocates memory for an array using malloc
+3-array_range.c: write a function that creates an array of integers

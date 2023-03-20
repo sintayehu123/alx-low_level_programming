1bc566ea13dbcf8480bd692dd0cafc4dc1ca6f70
sintayehu123 <sintayehumolla@gmail.com>
2023-03-20 08:31:32 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,15 +1,12 @@
-#include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * main - Entery point
- *
- * Return: 0 (SUCESS)
- */
+  * main - Prints the file name of the program
+  *
+  * Return: Always zero
+  */
 int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
 }
-

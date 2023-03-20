b159a8a2cf503ddd07f8b0a23825014ffcf07cf3
sintayehu123 <sintayehumolla@gmail.com>
2023-03-20 08:27:31 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,15 @@
+#include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entery point
+ *
+ * Return: 0 (SUCESS)
+ */
 int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
 }
+

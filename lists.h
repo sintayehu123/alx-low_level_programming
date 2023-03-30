c97f2d79b39c653459d98d553a4d260eaeb0d8e1
sintayehu123 <sintayehumolla@gmail.com>
2023-03-30 10:43:04 -0700

    lists.h

--- lists.h
+++ lists.h
@@ -1,8 +1,9 @@
-#ifndef LISTS
-#define LISTS
+#ifndef LISTS_H
+#define LISTS_H
+
 #include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -11,13 +12,12 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
  */
 typedef struct list_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
+char *str;
+unsigned int len;
+struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);

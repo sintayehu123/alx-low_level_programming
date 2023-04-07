454441d680ff301bf0790152098723b67d44c9ca
sintayehu123 <sintayehumolla@gmail.com>
2023-04-07 09:09:56 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,23 +9,23 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int a = 0, c = 1;
-	int i;
+	unsigned int num = 0, mult = 1;
+	int len;
 
 	if (b == '\0')
 		return (0);
 
-	for (i = 0; b[i];)
-		i++;
+	for (len = 0; b[len];)
+		len++;
 
-	for (i -= 1; i >= 0; i--)
+	for (len -= 1; len >= 0; len--)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (b[len] != '0' && b[len] != '1')
 			return (0);
 
-		a += (b[i] - '0') * c;
-		c *= 2;
+		num += (b[len] - '0') * mult;
+		mult *= 2;
 	}
 
-	return (a);
+	return (num);
 }
